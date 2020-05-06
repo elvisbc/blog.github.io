b04b2a35689c458781d6673e326af992fe9f9aef
Elvis <elvisb_40@hotmail.com>
2020-05-06 16:21:39 -0500

    0.1.2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('js/sw.js')
-    .then((reg)=> console.log('service worker registered',reg))
-    .catch((err)=>console.log('service worked not registered',err));
-}
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
